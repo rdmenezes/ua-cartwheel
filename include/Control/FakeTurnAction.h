73ef3e92b0d56283fff0577bfe8ea425a59caf0c
thomasjwalsh@gmail.com <thomasjwalsh@gmail.com@056ef9b6-64eb-5a25-03ae-ee7ac61d2d91>
2011-02-18 22:44:06 +0000

    now with turning files

---
+++ include/Control/FakeTurnAction.h
@@ -0,0 +1,18 @@
+#ifndef _FAKETURNACTION_H_
+#define _FAKETURNACTION_H_
+
+#include "TomsAction.h"
+
+class FakeTurnAction : TomsAction{
+
+public:
+FakeTurnAction(){myChange = 0.0;}
+FakeTurnAction(double change){myChange = change;}
+void execute(CartWheel3D * cw);
+
+
+private:
+double myChange;
+};
+
+#endif
